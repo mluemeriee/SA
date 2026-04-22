351c89f99ab6fe930cc3c98dc95725d93443249f
Ann Sapatan <sapatanshanylann@example.com>
2026-04-23 07:14:40 +0800

    Add Dockerfile and gitignore

--- MobileApi/obj/Debug/net8.0/MobileApi.AssemblyInfo.cs
+++ MobileApi/obj/Debug/net8.0/MobileApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d771759cc35bb677618c642fbb2b30473cc0ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
